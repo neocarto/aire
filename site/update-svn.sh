15f7dd057f67a8a1dd6b71101304c0116d0b5c9d
okapi <okapi@a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd>
2010-12-06 20:40:18 +0000

    catapatate
    git-svn-id: http://code.lapatate.org/svn/trunk/sites/cartapatate-catapatate@890 a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd

--- site/update-svn.sh
+++ site/update-svn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-svn up --ignore-externals . src/cartapatate src/cartapatate/zig src/cartapatate/ploomap && site/console zig:install:update-public --init=true && site/console zig:install:build-openlayers && site/console zig:install:run-shrinksafe
+svn up --ignore-externals . src/cartapatate src/cartapatate/zig src/cartapatate/ploomap
+#&& site/console zig:install:update-public --init=true && site/console zig:install:build-openlayers && site/console zig:install:run-shrinksafe
