a7a9dc9fd54752c78a95a969d63cc711df83cc70
Okapi <okapi@lapatate.org>
2011-06-10 03:46:59 +0200

    aire

--- app/commit.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-git commit $*
-for i in vendor/{cartapatate,zig,jig,ploomap-server,ploomap-client}; do
-    cd $i
-    git commit $*
-    cd -
-done
--- app/status.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-git status
-for i in vendor/{cartapatate,zig,jig,ploomap-server,ploomap-client}; do
-    cd $i
-    git status
-    cd -
-done
