d84a27666c62855d666403aa1fcec4251d09403a
Projet AIRE <jf@geonef.fr>
2011-06-11 02:05:52 +0200

    AIRE packaging, OGR Ms output

--- app/rec.sh
+++ app/rec.sh
@@ -4,10 +4,10 @@ for i in vendor/{cartapatate,zig,jig,ploomap-server,ploomap-client}; do
     cd $i
     echo
     echo "*** Moved to $i"
-    $*
-    cd -
+    "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    cd - >/dev/null
 done
 
 echo
 echo "*** Moved back to $PWD"
-$*
+"$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
