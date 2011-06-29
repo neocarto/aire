6bc8b8a76aab6ff72175539649ac5ee7e5240891
Okapi <okapi@lapatate.org>
2011-06-29 15:43:40 +0200

    renamed Geonef\Aire -> Riate\Aire (old fix)

---
+++ src/Riate/AireBundle/RiateAireBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Riate\AireBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RiateAireBundle extends Bundle
+{
+}
--- src/Riate/AireBundle/View/MapCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Riate\AireBundle\View;
-
-use Geonef\Zig\View\Document\XHtml;
-
-class MapSet extends XHtml
-{
-}
