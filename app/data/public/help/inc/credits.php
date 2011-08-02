935995f95c1e4f644a6ec116809ea68e57ec16ae
root <root@cnrs3.nfrance.com>
2011-08-02 05:28:08 +0200

    Work of Nicolas on app/data/

--- app/data/public/help/inc/credits.php
+++ app/data/public/help/inc/credits.php
@@ -1,35 +1,69 @@
 ﻿<?php
 // FRANCAIS
 $title_fr ="Crédits";
+
 $content_fr = '<h1>AIRE 2.0</h1>';
 $content_fr .= '<p>Mise à jour : <b>30 juillet 2011</b></p>';
-$content_fr .= '<hr/>';
-$content_fr .= '<h1>Christine Zanin</h1>';
-$content_fr .= '<p><b>MCF- Université Paris Diderot</b><br/>';
+$content_fr .= '<br/>';
+
+$content_fr .= '<table><tr><td width="200px">';
+$content_fr .= '<h2>Christine Zanin</h2>';
+$content_fr .= '</td><td>';
+$content_fr .= '<b>MCF- Université Paris Diderot</b><br/>';
 $content_fr .= 'UMR 8504 Géographie-cités / UMS 2414 RIATE<br/>';
 $content_fr .= '<a href="mailto:christine.zanin@univ-paris-diderot.fr">christine.zanin@univ-paris-diderot.fr</a><br/>';
-$content_fr .= '+33.1.57.27.65.08</p>';
-$content_fr .= '<h1>&</h1>';
-$content_fr .= '<h1>Nicolas Lambert</h1>';
-$content_fr .= '<p><b>Ingénieur d\'études CNRS</b><br/>';
+$content_fr .= '+33.1.57.27.65.08';
+$content_fr .= '</td><td>';
+$content_fr .= '</tr></table>';
+
+$content_fr .= '<table><tr><td width="200px">';
+$content_fr .= '<h2>Nicolas Lambert</h2>';
+$content_fr .= '</td><td>';
+$content_fr .= '<b>Ingénieur d\'études CNRS</b><br/>';
 $content_fr .= 'UMS 2414 RIATE<br/>';
 $content_fr .= '<a href="mailto:nicolas.lambert@ums-riate.fr">nicolas.lambert@ums-riate.fr</a><br/>';
-$content_fr .= '+33.1.57.27.65.32<br/></p>';
+$content_fr .= '+33.1.57.27.65.32<br/>';
+$content_fr .= '</tr></table>';
+
+$content_fr .='<br/>';
+$content_fr .= '<h3><b><u>Développement</u> : Jean-François GIGAN <span class="txt">(';
+$content_fr .= '<a href="mailto:jf@geonef.fr">jf@geonef.fr</a>)</SPAN></h3>';
+
+$content_fr .= '<h4>Avec la participation de : <span class="txt"> ';
+$content_fr .= 'Ronan Ysebaert, Julie Bonneau et Henry Ciesielski</span></h4>';
+
+
+
 
 // ANGLAIS
 $title_en ='Credits';
-$content_en = '<h1>AIRE 2.0</h1>';
-$content_en .= '<p>Last update: <b>July 30th 2011</b></p>';
-$content_en .= '<hr/>';
 
-$content_en .= '<h1>Christine Zanin</h1>';
-$content_en .= '<p><b>Assistant- University Paris Diderot</b><br/>';
+$content_en = '<h1>AIRE 2.0</h1>';
+$content_en .= '<p>Mise à jour : <b>30 juillet 2011</b></p>';
+$content_en .= '<br/>';
+
+$content_en .= '<table><tr><td width="200px">';
+$content_en .= '<h2>Christine Zanin</h2>';
+$content_en .= '</td><td>';
+$content_en .= '<b>Assistant- University Paris Diderot</b><br/>';
 $content_en .= 'UMR 8504 Géographie-cités / UMS 2414 RIATE<br/>';
 $content_en .= '<a href="mailto:christine.zanin@univ-paris-diderot.fr">christine.zanin@univ-paris-diderot.fr</a><br/>';
-$content_en .= '+33.1.57.27.65.08</p>';
-$content_en .= '<h1>&</h1>';
-$content_en .= '<h1>Nicolas Lambert</h1>';
-$content_en .= '<p><b>Engineer in cartography and GIS (CNRS)</b><br/>';
+$content_en .= '+33.1.57.27.65.08';
+$content_en .= '</td><td>';
+$content_en .= '</tr></table>';
+
+$content_en .= '<table><tr><td width="200px">';
+$content_en .= '<h2>Nicolas Lambert</h2>';
+$content_en .= '</td><td>';
+$content_en .= '<b>Engineer in cartography and GIS (CNRS)</b><br/>';
 $content_en .= 'UMS 2414 RIATE<br/>';
 $content_en .= '<a href="mailto:nicolas.lambert@ums-riate.fr">nicolas.lambert@ums-riate.fr</a><br/>';
-$content_en .= '+33.1.57.27.65.32<br/></p>';
+$content_en .= '+33.1.57.27.65.32<br/>';
+$content_en .= '</tr></table>';
+$content_en .='<br/>';
+$content_en .= '<h3><b><u>Development</u> : Jean-François GIGAN <span class="txt">(';
+$content_en .= '<a href="mailto:jf@geonef.fr">jf@geonef.fr</a>)</SPAN></h3>';
+
+$content_en .= '<h4>Other contributions : <span class="txt"> ';
+$content_en .= 'Ronan Ysebaert, Julie Bonneau and Henry Ciesielski</span></h4>';
+
